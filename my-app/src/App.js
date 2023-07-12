7438c07d335bd647214735ded8c096936f26ef8c
Kefi228 <dippermuxa15@gmail.com>
2023-07-12 15:10:34 +0300

    added components and init style and image

---
+++ my-app/src/App.js
@@ -0,0 +1,24 @@
+import React from "react"
+import Header from "./components/Header"
+import Image from "./components/Image"
+import logo from "./img/logo.webp"
+
+class App extends React.Component {
+  helpText = "Help text"
+
+  render(){
+    return(<div className = "name">
+  <Header title = "Шапка сайта" />
+  <h1>{this.helpText}</h1>
+  <input placeholder={this.helpText} onClick ={this.inputClick} onMouseEnter = {this.mouseOver} />
+  <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
+  <Image image = {logo}/>
+  <img src={logo} />
+  </div>)
+  }
+
+  inputClick(){console.log("Clicked")}
+  mouseOver(){console.log("Mouse Over")}
+}
+
+export default App
