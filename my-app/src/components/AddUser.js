38822f0e1b9875e4904a9ddcc2c13d5a1e16a149
Kefi228 <dippermuxa15@gmail.com>
2023-07-13 00:05:37 +0300

    edit and deleting user

--- my-app/src/components/AddUser.js
+++ my-app/src/components/AddUser.js
@@ -1,6 +1,7 @@
 import React from "react"
 
 class AddUser extends React.Component {
+    userAdd = {}
     constructor(props){
         super(props)
         this.state = {
@@ -22,13 +23,16 @@ class AddUser extends React.Component {
             <input type="checkbox" id="isHappy" onChange ={(event) => this.setState({isHappy: event.target.checked})}/>
             <button type="button" onClick={() =>{
                 this.myForm.reset() 
-                this.props.onAdd({
-                firstname: this.state.firstname,
-                lastname: this.state.lastname,
-                bio: this.state.bio,
-                age: this.state.age,
-                isHappy: this.state.isHappy
-            })}
+                this.userAdd ={
+                    firstname: this.state.firstname,
+                    lastname: this.state.lastname,
+                    bio: this.state.bio,
+                    age: this.state.age,
+                    isHappy: this.state.isHappy
+                }
+                if(this.props.user)
+                    this.userAdd.id = this.props.user.id
+                this.props.onAdd(this.userAdd)}
         }>Добавить</button>
         </form>
         )
