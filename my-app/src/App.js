71485a6c192515e6c7603ebc0436c42932ed7e95
Kefi228 <dippermuxa15@gmail.com>
2023-07-12 15:59:34 +0300

    set states

--- my-app/src/App.js
+++ my-app/src/App.js
@@ -4,21 +4,33 @@ import Image from "./components/Image"
 import logo from "./img/logo.webp"
 
 class App extends React.Component {
-  helpText = "Help text"
+    constructor(props){
+        super(props)
+        this.state = {
+            helpText: "Help text",
+            userData: ""
+        }
+        this.inputClick = this.inputClick.bind(this)
+    }
 
-  render(){
-    return(<div className = "name">
-  <Header title = "Шапка сайта" />
-  <h1>{this.helpText}</h1>
-  <input placeholder={this.helpText} onClick ={this.inputClick} onMouseEnter = {this.mouseOver} />
-  <p>{this.helpText === "Help text!" ? "Yes" : "No"}</p>
-  <Image image = {logo}/>
-  <img src={logo} />
-  </div>)
-  }
+    render(){
+        return(<div className = "name">
+    <Header title = "Шапка сайта" />
+    <h1>{this.state.helpText}</h1>
+    <h2>{this.state.userData}</h2>
+    <input placeholder={this.state.helpText} onChange = {event => this.setState({userData: event.target.value})}
+         onClick ={this.inputClick} onMouseEnter = {this.mouseOver} />
+    <p>{this.state.helpText === "Help text!" ? "Yes" : "No"}</p>
+    <Image image = {logo}/>
+    <img src={logo} />
+    </div>)
+    }
 
-  inputClick(){console.log("Clicked")}
-  mouseOver(){console.log("Mouse Over")}
+    inputClick(){
+        this.setState({helpText: "Changed"})
+        console.log("Clicked")
+    }
+    mouseOver(){console.log("Mouse Over")}
 }
 
 export default App
